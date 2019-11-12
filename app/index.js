2284d537233fea287f9e0a2b4b1793fcb72eebdf
nirkaufman <nirkaufman@gmail.com>
2019-11-11 23:27:20 -0500

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,49 @@
+import { app, BrowserWindow } from 'electron';
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,    
+    webPreferences: {nodeIntegration: true}
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// On OS X it's common to re-create a window in the app when the
+// dock icon is clicked and there are no other windows open.
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ app/renderer/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <h1>Snippets. With Electron & React</h1>;
+
+export default App;
